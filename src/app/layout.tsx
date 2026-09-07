907e9023ba02c7aa257feb683683327be7e22e96
Sujeet Sharma <artisticvisions003@gmail.com>
2026-09-07 11:40:25 +0530

    Import Script component from next/script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Playfair_Display, Inter, Noto_Serif_SC } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
